b5b5729bcfe7188855c554b6d68e6a414fd3c1a3
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2021-12-02 21:14:54 +0200

    errorPage

--- Frontend/react-app/src/NotFound.js
+++ Frontend/react-app/src/NotFound.js
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom"
+import SmartToyOutlinedIcon from '@mui/icons-material/SmartToyOutlined';
+import { Avatar, createMuiTheme,FormControlLabel,ThemeProvider } from '@mui/material';
+
+const theme=createMuiTheme({
+  palette:{
+   primary:{
+     main:'#be8b14'
+    },
+    secondary:{
+      main:'#000000'
+  }
+}
+})
+
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Sorry</h2>
-      <p>That page cannot be found</p>
+    <ThemeProvider theme={theme}>
+    
+    <div align="center" className="not-found" style={{margin:"150px auto"}}>
+      <SmartToyOutlinedIcon color="primary" style={{fontSize:"200"}}/>
+      <h2>ERROR404: Page not found</h2>
+      <p>This page doesn't exist on our site, make sure you typed the correct url</p>
       <a href="/">Back to the homepage...</a>
     </div>
+    </ThemeProvider>
   );
 }
  
