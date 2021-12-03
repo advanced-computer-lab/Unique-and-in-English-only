299e3a4e6651410fd3466fd8c165a0292e41af71
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2021-12-03 15:45:25 +0200

    createFlight validation

--- Frontend/react-app/src/createFlight.js
+++ Frontend/react-app/src/createFlight.js
@@ -18,6 +18,8 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { useParams } from "react-router-dom";
 import { PromiseProvider } from 'mongoose';
+import InputAdornment from '@mui/material/InputAdornment';
+
 
 
 
@@ -53,18 +55,48 @@ export default function CreateFlight() {
   const classes = useStyles();
   const [buttonSuccessPopup, setButtonSuccessPopup] = useState(false);
   const [buttonFailurePopup, setButtonFailurePopup] = useState(false);
-  
-  const [depTimeValidate, setdepTimeValidate] = useState("");
-  const [depTimeValidateFlag, setdepTimeValidateFlag] = useState(false);
+
+  const [flightNumberValidate, setflightNumberValidate] = useState("");
+  const [flightNumberValidateFlag, setflightNumberValidateFlag] = useState(false);
+
+  const [tripDurationValidate, settripDurationValidate] = useState("");
+  const [tripDurationValidateFlag, settripDurationValidateFlag] = useState(false);
 
   const [arrTimeValidate, setarrTimeValidate] = useState("");
   const [arrTimeValidateFlag, setarrTimeValidateFlag] = useState(false);
 
+  const [depTimeValidate, setdepTimeValidate] = useState("");
+  const [depTimeValidateFlag, setdepTimeValidateFlag] = useState(false);
+  
+  const [businessSeatsValidate, setbusinessSeatsValidate] = useState("");
+  const [businessSeatsValidateFlag, setbusinessSeatsValidateFlag] = useState(false);
+
   const [economySeatsValidate, seteconomySeatsValidate] = useState("");
   const [economySeatsValidateFlag, seteconomySeatsValidateFlag] = useState(false);
 
-  const [businessSeatsValidate, setbusinessSeatsValidate] = useState("");
-  const [businessSeatsValidateFlag, setbusinessSeatsValidateFlag] = useState(false);
+  const [depPortValidate, setdepPortValidate] = useState("");
+  const [depPortValidateFlag, setdepPortValidateFlag] = useState(false);
+
+  const [arrPortValidate, setarrPortValidate] = useState("");
+  const [arrPortValidateFlag, setarrPortValidateFlag] = useState(false);
+
+  const [depTerminalValidate, setdepTerminalValidate] = useState("");
+  const [depTerminalValidateFlag, setdepTerminalValidateFlag] = useState(false);
+  
+  const [arrTerminalValidate, setarrTerminalValidate] = useState("");
+  const [arrTerminalValidateFlag, setarrTerminalValidateFlag] = useState(false);
+
+  const [businessPriceValidate, setbusinessPriceValidate] = useState("");
+  const [businessPriceValidateFlag, setbusinessPriceValidateFlag] = useState(false);
+  
+  const [economyPriceValidate, seteconomyPriceValidate] = useState("");
+  const [economyPriceValidateFlag, seteconomyPriceValidateFlag] = useState(false);
+
+  const [baggageAllowanceValidate, setbaggageAllowanceValidate] = useState("");
+  const [baggageAllowanceValidateFlag, setbaggageAllowanceValidateFlag] = useState(false);
+
+  
+  
 
 
 
@@ -94,7 +126,7 @@ export default function CreateFlight() {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
   const [open1, setOpen1] = React.useState(false);
-  const paperStyle={padding:20, height:'1300px',width:600,margin:"150px auto",minheight: '1300px'}
+  const paperStyle={padding:20, height:'1500px',width:600,margin:"150px auto",minheight: '1300px'}
   const avatarStyle={backgroundColor:'#be8b14'}
   const handleClick1 = () => {
     setOpen1(true);
@@ -140,13 +172,34 @@ export default function CreateFlight() {
     var flag2=true
     var flag3=true
     var flag4=true
-   
+    var flag5=true
+
+   setflightNumberValidateFlag(false)
+   settripDurationValidateFlag(false)
     setdepTimeValidateFlag(false)
     setarrTimeValidateFlag(false)
     seteconomySeatsValidateFlag(false)
     setbusinessSeatsValidateFlag(false)
-    
-    
+    setdepPortValidateFlag(false)
+    setarrPortValidateFlag(false)
+    setdepTerminalValidateFlag(false)
+    setarrTerminalValidateFlag(false)
+    setbusinessPriceValidateFlag(false)
+    seteconomyPriceValidateFlag(false)
+    setbaggageAllowanceValidateFlag(false)
+
+    setflightNumberValidate('')
+    settripDurationValidate('')
+    setdepPortValidate('')
+    setarrPortValidate('')
+    setdepTerminalValidate('')
+    setarrTerminalValidate('')
+    setbusinessPriceValidate('')
+    seteconomyPriceValidate('')
+    setbaggageAllowanceValidate('')
+
+
+
 
     var today = new Date();
     today.setHours(0,0,0,0)
@@ -157,6 +210,7 @@ export default function CreateFlight() {
     if(values.DepartureTime!=''){
       var depDate = new Date(values.DepartureTime)
     }
+    if(values.DepartureTime!='' && values.ArrivalTime!=''){
     if(depDate.getTime()<today.getTime()){
       setdepTimeValidate("Departure date has already passed")
       setdepTimeValidateFlag(true)
@@ -169,7 +223,7 @@ export default function CreateFlight() {
     }
     if(arrDate.getTime()<depDate.getTime()){
       console.log("hhh")
-      setarrTimeValidate("arrival date can`t be before departure date")
+      setarrTimeValidate("Arrival date can`t be before departure date")
       setarrTimeValidateFlag(true)
       flag2=false
     }
@@ -177,8 +231,15 @@ export default function CreateFlight() {
       flag2=true
       setarrTimeValidate("")
     }
-    if(values.EconomySeatsNumber<0){
-      seteconomySeatsValidate("number of seats can`t be negative")
+  }else{
+    setdepTimeValidate("Departure date can't be empty")
+    setdepTimeValidateFlag(true)
+    setarrTimeValidate("Arrival date can't be empty")
+      setarrTimeValidateFlag(true)
+
+  }
+    if(values.EconomySeatsNumber<=0){
+      seteconomySeatsValidate("Number of seats must be positive")
       seteconomySeatsValidateFlag(true)
       flag3=false
     }
@@ -186,8 +247,8 @@ export default function CreateFlight() {
       flag3=true
       seteconomySeatsValidate("")
     }
-    if(values.BuisnessSeatsNumber<0){
-      setbusinessSeatsValidate("number of seats can`t be negative")
+    if(values.BuisnessSeatsNumber<=0){
+      setbusinessSeatsValidate("Number of seats must be positive")
       flag4=false
       setbusinessSeatsValidateFlag(true)
     }
@@ -196,7 +257,79 @@ export default function CreateFlight() {
       setbusinessSeatsValidate("")
     }
     console.log(economySeatsValidate)
-    if(flag1&&flag2&&flag3&&flag4){
+
+    if(values.FlightNumber.length<3){
+      setflightNumberValidate("Flight Number must be atleast 3 characters long")
+      setflightNumberValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.TripDuration<=0){
+      settripDurationValidate("Trip Duration must be positive")
+      settripDurationValidateFlag(true)
+      flag5=false
+    }
+
+    
+    if(!values.ArrivalPort.charAt(0).match(/[a-z]/i)
+    || !values.ArrivalPort.charAt(1).match(/[a-z]/i)|| !values.ArrivalPort.charAt(2).match(/[a-z]/i)){
+      setarrPortValidate("Arrival Port must contain letters only")
+      setarrPortValidateFlag(true)
+      flag5=false
+    }
+    if(values.ArrivalPort.length!=3 ){
+      setarrPortValidate("Arrival Port must be 3 letters long")
+      setarrPortValidateFlag(true)
+      flag5=false
+    }
+
+    if(!values.DeparturePort.charAt(0).match(/[a-z]/i)
+    || !values.DeparturePort.charAt(1).match(/[a-z]/i)|| !values.DeparturePort.charAt(2).match(/[a-z]/i)){
+      setdepPortValidate("Departure Port must contain letters only ")
+      setdepPortValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.DeparturePort.length!=3){
+      setdepPortValidate("Departure Port must be 3 letters long")
+      setdepPortValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.DepartureTerminal<=0){
+      setdepTerminalValidate("Departure Terminal must be positive")
+      setdepTerminalValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.ArrivalTerminal<=0){
+      setarrTerminalValidate("Arrival Terminal must be positive")
+      setarrTerminalValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.BusinessPrice<=0){
+      setbusinessPriceValidate("Business Price must be positive")
+      setbusinessPriceValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.EconomyPrice<=0){
+      seteconomyPriceValidate("Economy Price must be positive")
+      seteconomyPriceValidateFlag(true)
+      flag5=false
+    }
+
+    if(values.BaggageAllowance<=0){
+      setbaggageAllowanceValidate("Baggae Allowance must be positive")
+      setbaggageAllowanceValidateFlag(true)
+      flag5=false
+    }
+
+
+
+
+    if(flag1&&flag2&&flag3&&flag4&&flag5){
       event.preventDefault();
       axios.post('http://localhost:150/flight/createflight', values)
         .then(function (response) {
@@ -209,6 +342,8 @@ export default function CreateFlight() {
           //setButtonFailurePopup(true)
           handleClick2();
         });
+    }else{
+      setOpen2(true)
     }
   }
   
@@ -239,6 +374,8 @@ export default function CreateFlight() {
          minLength="3"
          id="FlightNumber"
          value={values.FlightNumber} onChange={set('FlightNumber')} 
+         helperText={flightNumberValidate}
+        error={flightNumberValidateFlag}
          
          />
          </Grid>
@@ -247,6 +384,9 @@ export default function CreateFlight() {
 
           <h2 style={{color:"#be8b14"}}>Trip Duration:</h2>
           <TextField
+          InputProps={{
+            endAdornment: <InputAdornment  position="end">mins</InputAdornment>,
+          }}
           type="text"
           id="TripDuration"
           variant="standard"
@@ -258,6 +398,8 @@ export default function CreateFlight() {
           required 
           value={values.TripDuration} 
           onChange={set('TripDuration')}
+          helperText={tripDurationValidate}
+        error={tripDurationValidateFlag}
           />
         </Grid>
 
@@ -266,7 +408,7 @@ export default function CreateFlight() {
          
 
          <Grid item xs={6}  >
-        <h2 style={{color:"#be8b14"}}>Arrival Time:</h2>
+        <h2 style={{color:"#be8b14"}}>Arrival Date:</h2>
          <TextField
          type="date"
          id="ArrivalTime"
@@ -278,11 +420,12 @@ export default function CreateFlight() {
         value={values.ArrivalTime} onChange={set('ArrivalTime')}
         helperText={arrTimeValidate}
         error={arrTimeValidateFlag}
+        
          />
          </Grid>
 
          <Grid item xs={6} align="center"> 
-         <h2 style={{color:"#be8b14"}}>Departure Time:</h2>
+         <h2 style={{color:"#be8b14"}}>Departure Date:</h2>
          
          <TextField 
          type="date"
@@ -351,6 +494,8 @@ export default function CreateFlight() {
         required 
         value={values.DeparturePort} 
         onChange={set('DeparturePort')}
+        helperText={depPortValidate}
+        error={depPortValidateFlag}
          />
          </Grid>
 
@@ -368,6 +513,8 @@ export default function CreateFlight() {
         required 
         value={values.ArrivalPort} 
         onChange={set('ArrivalPort')}
+        helperText={arrPortValidate}
+        error={arrPortValidateFlag}
          />
          </Grid>
 
@@ -385,6 +532,8 @@ export default function CreateFlight() {
         required 
         value={values.DepartureTerminal} 
         onChange={set('DepartureTerminal')}
+        helperText={depTerminalValidate}
+        error={depTerminalValidateFlag}
          />
          </Grid>
 
@@ -402,6 +551,8 @@ export default function CreateFlight() {
         required 
         value={values.ArrivalTerminal} 
         onChange={set('ArrivalTerminal')}
+        helperText={arrTerminalValidate}
+        error={arrTerminalValidateFlag}
          />
          </Grid>
 
@@ -419,6 +570,8 @@ export default function CreateFlight() {
         required 
         value={values.BusinessPrice} 
         onChange={set('BusinessPrice')}
+        helperText={businessPriceValidate}
+        error={businessPriceValidateFlag}
          />
          </Grid>
 
@@ -436,12 +589,17 @@ export default function CreateFlight() {
         required 
         value={values.EconomyPrice} 
         onChange={set('EconomyPrice')}
+        helperText={economyPriceValidate}
+        error={economyPriceValidateFlag}
          />
          </Grid>
 
          <Grid item xs={6} >
         <h2 style={{color:"#be8b14"}}>Baggage Allowance:</h2>
          <TextField
+         InputProps={{
+          endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+        }}
          type="text"
          id="BaggageAllowance"
          variant="standard"
@@ -453,6 +611,8 @@ export default function CreateFlight() {
         required 
         value={values.BaggageAllowance} 
         onChange={set('BaggageAllowance')}
+        helperText={baggageAllowanceValidate}
+        error={baggageAllowanceValidateFlag}
          />
          </Grid>
 
@@ -505,7 +665,7 @@ export default function CreateFlight() {
       
       <Snackbar open={open2} autoHideDuration={6000} onClose={handleClose2}>
         <Alert onClose={handleClose2} severity="error" sx={{ width: '100%' }}>
-          There is an error!
+          Flight not created!
         </Alert>
       </Snackbar>
       </Stack>
