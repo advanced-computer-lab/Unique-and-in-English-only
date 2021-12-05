2602d981ae7d2a6271aee761f288d076d957fce2
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2021-12-06 01:46:10 +0200

    submission

--- Frontend/react-app/src/searchFlightDiv.js
+++ Frontend/react-app/src/searchFlightDiv.js
@@ -15,7 +15,12 @@ import InputLabel from '@mui/material/InputLabel';
 import { Container } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import { Avatar, createMuiTheme,FormControlLabel,ThemeProvider } from '@mui/material';
+
+
 
 const theme=createMuiTheme({
     palette:{
@@ -46,6 +51,49 @@ const cabinOptions = [
 
 function SearchFlightDiv() {
 
+    const [From, setFrom] = useState("");
+     const [FromFlag, setFromFlag] = useState(false);
+
+     const [To, setTo] = useState("");
+     const [ToFlag, setToFlag] = useState(false);
+
+     const [Adults, setAdults] = useState("");
+     const [AdultsFlag, setAdultsFlag] = useState(false);
+
+     const [Children, SetChildren] = useState("");
+     const [ChildrenFlag, setChildrenFlag] = useState(false);
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+    
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+      });
+      const [open1, setOpen1] = React.useState(false);
+      const avatarStyle={backgroundColor:'#be8b14'}
+      const handleClick1 = () => {
+        setOpen1(true);
+      };
+    
+      const handleClose1 = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen1(false);
+      };
+      const [open2, setOpen2] = React.useState(false);
+    
+      const handleClick2 = () => {
+        setOpen2(true);
+      };
+    
+      const handleClose2 = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen2(false);
+      };
+
     const set = name => {
         return ({ target: { value } }) => {
             setValues(oldValues => ({ ...oldValues, [name]: value }));
@@ -64,11 +112,12 @@ function SearchFlightDiv() {
 
     });
 
+    if(children)
+
     const [flyingFrom, setFlyingFrom] = useState(null);
     const [flyingTo, setFlyingTo] = useState(null);
     const history = useHistory();
     const confirmSearch = async (event) => {
-
         const element = (
             <div>
                 <h1>Hello, world!</h1>
@@ -85,6 +134,17 @@ function SearchFlightDiv() {
             flight[key] = values[key];
         }
         event.preventDefault();
+        const outboundDateObj = new Date(flight.outboundDate);
+        const returnDateObj = new Date(flight.returnDate);
+        const invalidFlightDateFlag = outboundDateObj.getTime>returnDateObj.getTime
+        const f1= new Date()
+        f1.setHours(0,0,0,0)
+        const flightDatePassed= f1.getTime()>outboundDateObj.getTime();
+        
+        if(flyingFrom==flyingTo ||flight.adults==0 ||  invalidFlightDateFlag ||flightDatePassed){
+            setOpen2(true);
+            return;
+        }
         axios.post('http://localhost:150/flight/searchFlightPassenger', flight)
             .then(
                 history.push("/flightSelection")
@@ -92,6 +152,7 @@ function SearchFlightDiv() {
             .catch(function (error) {
                 console.log(error);
             });
+        
 
 
     }
@@ -134,6 +195,8 @@ function SearchFlightDiv() {
                     id="flying-to-box"
                     options={airports}
                     value={flyingTo}
+                    error={ToFlag}
+                    Texthelper={To}
                     defaultValue={{ airportName: "CAI", city: "Cairo" }}
                     onChange={(_, newVal) => {
                         setFlyingTo(newVal);
@@ -150,7 +213,10 @@ function SearchFlightDiv() {
          <Grid item xs={3}>
              <h5>Outbound Date</h5>
                 
-                <TextField id="outboundDate" value={values.outboundDate} onChange={set('outboundDate')} type="date" variant="standard"> </TextField>
+                <TextField id="outboundDate" value={values.outboundDate} onChange={set('outboundDate')} type="date" variant="standard"
+                Texthelper={From}
+                error={FromFlag}
+                > </TextField>
          </Grid>
          <Grid item xs={3}>
          <h5>Return Date</h5>
@@ -166,6 +232,8 @@ function SearchFlightDiv() {
             variant="standard"
             value={values.adults}
             onChange={set('adults')}
+            error={AdultsFlag}
+            Texthelper={Adults}
             InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -182,6 +250,8 @@ function SearchFlightDiv() {
          variant="standard"
                         value={values.children}
                         onChange={set('children')}
+                        error={ChildrenFlag}
+                        Texthelper={Children}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -228,6 +298,23 @@ function SearchFlightDiv() {
                 </Grid>
                 </Paper>
                 </Grid>
+
+                <Stack spacing={2} sx={{ width: '100%' }}>
+      
+      <Snackbar open={open1} autoHideDuration={6000} onClose={handleClose1}>
+        <Alert onClose={handleClose1} severity="success" sx={{ width: '100%' }}>
+          The flight is created!
+        </Alert>
+      </Snackbar>
+      </Stack>
+      <Stack spacing={2} sx={{ width: '100%' }}>
+      
+      <Snackbar open={open2} autoHideDuration={6000} onClose={handleClose2}>
+        <Alert onClose={handleClose2} severity="error" sx={{ width: '100%' }}>
+          Invalid input!
+        </Alert>
+      </Snackbar>
+      </Stack>
                 
         </Container>
         </ThemeProvider>
