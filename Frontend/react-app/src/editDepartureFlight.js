0cc34f75adcd99d54cd78b21260433a7c97144b5
Farouk Amr <50875890+FaroukAmr@users.noreply.github.com>
2021-12-23 13:58:24 +0200

    m

--- Frontend/react-app/src/editDepartureFlight.js
+++ Frontend/react-app/src/editDepartureFlight.js
@@ -3,10 +3,12 @@ import ResponsiveAppBar from "./ResponsiveAppBar";
 import Card from "./Card";
 import "./userHome.css";
 import FlightSearch from "./searchFlightDiv.js";
+import EditLocationOutlinedIcon from '@mui/icons-material/EditLocationOutlined';
 const EditDepartureFlight = () => {
     return (
-        <div className="mainContainer" >
-            <h1 > Search for a new flight</h1>
+        <div className="mainContainer" align="center" >
+            <EditLocationOutlinedIcon color="primary" style={{ fontSize: "150" }}/>
+            <h1 style={{color:"#bd8b13"}}> Search for a new flight</h1>
             <FlightSearch />
             
 
